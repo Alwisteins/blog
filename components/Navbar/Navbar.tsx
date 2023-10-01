8f296207908bcae446e7ed7b913c8a0db56e804d
unknown <alwyyourbae@gmail.com>
2023-09-30 19:58:52 -0700

    add categories filter modal(complete)

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
+import { FcFilledFilter } from "react-icons/fc";
 
 type Urls = { target: string; title: string }[];
 
@@ -45,19 +46,43 @@ const CategoriesModal = [
 ];
 
 export default function Navbar() {
+  // handle popup categories
   const [showCategories, setShowCategories] = useState(false);
   const toggleCategories = () => {
     setShowCategories(!showCategories);
   };
+
+  // handle status checkbox
+  const [checkedItems, setCheckedItems] = useState<string[]>([]);
+  const handleCheckboxChange = (itemList: string) => {
+    //check if selected item is already on state or not
+    checkedItems.includes(itemList)
+      ? setCheckedItems(checkedItems.filter((item) => item !== itemList))
+      : setCheckedItems([...checkedItems, itemList]);
+  };
+
+  //handle form submit
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    //show selected filtered items
+    console.log("selected filter: ", checkedItems);
+  };
   return (
     <>
       <nav className="flex justify-between items-center h-14 px-3">
-        <h1 className=" font-bold text-lg">SteinsLibrary</h1>
+        <h1 className="font-bold text-lg">SteinsLibrary</h1>
         <ul className="flex space-x-4">
           {urls.map((url) => (
             <li key={url.title} className="list-none">
-              {url.title == "Categories" ? (
-                <button onClick={toggleCategories}>{url.title}</button>
+              {url.title === "Categories" ? (
+                <button
+                  onClick={toggleCategories}
+                  className={`${
+                    showCategories ? "text-amber-600" : "text-black"
+                  }`}
+                >
+                  {url.title}
+                </button>
               ) : (
                 <Link href={url.target}>{url.title}</Link>
               )}
@@ -66,19 +91,45 @@ export default function Navbar() {
         </ul>
       </nav>
       {showCategories && (
-        <div className=" bg-white/[0.3] backdrop-blur-sm w-full h-screen left-0 absolute">
-          <div className="flex flex-col m-auto w-[800px] h-[450px] bg-white border-2 border-black rounded-lg">
+        <div className="bg-white/30 backdrop-blur-sm w-full h-screen left-0 absolute">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col m-auto w-[800px] h-[450px] bg-white border-2 border-black rounded-lg"
+          >
             {CategoriesModal.map((item) => (
               <div key={item.title} className="m-4">
-                <h1 className=" text-center">{item.title}</h1>
+                <h1 className="text-center text-lg font-medium">
+                  {item.title}
+                </h1>
                 {item.list.map((itemList, i) => (
-                  <p key={i} className="inline-block m-2 p-2 bg-zinc-800 text-white rounded-3xl">
-                    {itemList}
-                  </p>
+                  <div
+                    key={i}
+                    className={`m-2 p-2 inline-block text-white rounded-3xl ${
+                      checkedItems.includes(itemList)
+                        ? "bg-amber-600"
+                        : "bg-zinc-800 hover:bg-amber-700"
+                    }`}
+                  >
+                    <input
+                      type="checkbox"
+                      className="hidden"
+                      id={itemList}
+                      name={itemList}
+                      onChange={() => handleCheckboxChange(itemList)}
+                    />
+                    <label htmlFor={itemList}>{itemList}</label>
+                  </div>
                 ))}
               </div>
             ))}
-          </div>
+            <button
+              type="submit"
+              className=" text-sm flex items-center justify-center relative top-20 w-20 m-auto py-1 px-2 rounded-3xl border-2 bg-zinc-800 text-white hover:bg-amber-600 hover:border-amber-700"
+            >
+              <FcFilledFilter />
+              Filter
+            </button>
+          </form>
         </div>
       )}
     </>
